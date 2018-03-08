080440c620e1fe7c4824f11730b1fe372ec3b11c
jeffsantaines <javacodegod@gmail.com>
2018-03-09 08:17:27 +1300

    Flipped all the cards facing down

--- js/app.js
+++ js/app.js
@@ -2,6 +2,18 @@
  * Create a list that holds all of your cards
  */
 
+//Selects all the 'array' descendants 'li'
+const cards = document.querySelectorAll('.deck li');
+//const card_state= ['card','card-open-show','card-match'];
+
+//array of the icons' class
+const card_icon = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bomb',
+    'fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bomb'];
+
+//const qty_per_card= 2; 
+
+//selecting the element with className restart
+//const restart = document.getElementsByClassName('restart');
 
 /*
  * Display the cards on the page
@@ -10,9 +22,19 @@
  *   - add each card's HTML to the page
  */
 
+//document.querySelector('.restart').addEventListener("click", flipCardsFacingDown);
+
+//when the windows load flip the cards to face down
+window.onload = function flipCardsFacingDown() {
+    cards.forEach(function (card) {
+        card.className = 'card';
+    });
+}
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length;
+    let temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -21,10 +43,7 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
-    return array;
 }
-
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -36,3 +55,5 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
